e09905089622b2f8205a0fdc4c604509bc77cf79
Samuel Garcia Moreira <samuelgarciamoreira1985@gmail.com>
2025-08-02 14:36:40 -0300

    LUNCH_KING - componente mainHeader

--- LUNCH_KING/lunch_king/src/components/FastMenu/FastMenu.jsx
+++ LUNCH_KING/lunch_king/src/components/FastMenu/FastMenu.jsx
@@ -3,7 +3,11 @@ import "./FastMenu.css"
 //IMAGENS
 import logoSystem from "../../assets/images/logo-system.png"
 import lunchFastMenu from "../../assets/images/Lunch_FastMenu.png"
-
+import portionFastMenu from "../../assets/images/Portion_FastMenu.png"
+import pastriesFastMenu from "../../assets/images/Pastries_FastMenu.png"
+import dessertsFastMenu from "../../assets/images/Desserts_FastMenu.png"
+import industrialFastMenu from "../../assets/images/Industrial_FastMenu.png"
+import drinksFastMenu from "../../assets/images/Drinks_FastMenu.png"
 
 const FastMenu = () => {
 
@@ -17,27 +21,27 @@ const FastMenu = () => {
 
         <div className="items-fast-menu">
             <div className="details-items-fast-menu">
-                <a href="#"><img src={lunchFastMenu} alt="" /></a>
+                <a href="#"><img src={lunchFastMenu} alt="imagem_lanche" /></a>
                 <p>Lanches</p>
             </div>
             <div className="details-items-fast-menu">
-                <a href="#"><img src="#" alt="" /></a>
+                <a href="#"><img src={portionFastMenu} alt="imagem_porções" /></a>
                 <p>Porções</p>
             </div>
             <div className="details-items-fast-menu">
-                <a href="#"><img src="#" alt="" /></a>
+                <a href="#"><img src={pastriesFastMenu} alt="imagem_pastéis" /></a>
                 <p>Pastéis</p>
             </div>
             <div className="details-items-fast-menu">
-                <a href="#"><img src="#" alt="" /></a>
-                <p>Spbremesas</p>
+                <a href="#"><img src={dessertsFastMenu} alt="imagem_sobremesas" /></a>
+                <p>Sobremesas</p>
             </div>
             <div className="details-items-fast-menu">
-                <a href="#"><img src="#" alt="" /></a>
+                <a href="#"><img src={industrialFastMenu} alt="imagem_indistrializados" /></a>
                 <p>Industriais</p>
             </div>
             <div className="details-items-fast-menu">
-                <a href="#"><img src="#" alt="" /></a>
+                <a href="#"><img src={drinksFastMenu} alt="imagem_bebidas" /></a>
                 <p>Bebidas</p>
             </div>
         </div>
--- LUNCH_KING/lunch_king/src/components/MainHeader/MainHeader.jsx
+++ LUNCH_KING/lunch_king/src/components/MainHeader/MainHeader.jsx
@@ -1,5 +1,7 @@
 // CSS
 import "./MainHeader.css"
+//IMAGENS
+import logoSystemMainHeader from "../../assets/images/logo-system.png"
 
 const MainHeader = () => {
 
@@ -7,32 +9,30 @@ const MainHeader = () => {
 
     <div className="main-header-container">
         <div className="icon-header">
-            <img src="#" alt="Ícone do sistema" />
+            <a href=""><img src={logoSystemMainHeader} alt="Ícone do sistema" /></a>
         </div>
 
-        <div className="search-main">
-
-            <form className="form-inputs-header">
-                <select name="n-search-options" id="id-search-options">
-                    <option value="userHeaderMain">USUÁRIOS</option>
-                    <option value="productHeaderMain">PRODUTOS</option>
-                    <option value="commandHeaderMain">COMANDAS</option>
-                    <option value="saleHeaderMain">VENDAS</option>
-                </select>
-
-                <input type="text"
-                id="id-search-fast"
-                name="n-search-fast"
-                placeholder="Digite o valor da pesquisa..."
-                required
-                max={50}
-                min={1}    
-                />
-                <input type="submit" value="Buscar"/>
+            <form className="form-inputs-search">
+                <fieldset>
+                    <legend>Selecione um padrão de busca</legend>
+                    <select name="n-search-options" id="id-search-options">
+                        <option value="userHeaderMain">USUÁRIOS</option>
+                        <option value="productHeaderMain">PRODUTOS</option>
+                        <option value="commandHeaderMain">COMANDAS</option>
+                        <option value="saleHeaderMain">VENDAS</option>
+                    </select>
+                    <input type="text"
+                    id="id-search-fast"
+                    name="n-search-fast"
+                    placeholder="Digite o valor da pesquisa..."
+                    required
+                    max={50}
+                    min={1}
+                    />
+                    <input type="submit" id="id-btn-buscar" value="Buscar"/>
+                </fieldset>
             </form>
         </div>
-
-    </div>
 
   )
 }
