c1b340697c69ebd64214025313a7df91ac630228
Samuel Garcia Moreira <samuelgarciamoreira1985@gmail.com>
2025-09-08 16:54:50 -0300

    LUNCH_KING - gestao de comandas atualizado

--- LUNCH_KING/lunch_king/src/pages/Commands.jsx
+++ LUNCH_KING/lunch_king/src/pages/Commands.jsx
@@ -1,8 +1,14 @@
+//REACT
+import { Tooltip } from "@mui/material"
 //CSS
 import "./Commands.css"
 //ÍCONES
 import { PiMotorcycleFill } from "react-icons/pi"
-import { FaCartArrowDown } from "react-icons/fa"
+import { FaCartArrowDown,FaShoppingCart } from "react-icons/fa"
+import { FaNotesMedical,FaCandyCane } from "react-icons/fa6"
+import { MdLunchDining } from "react-icons/md" 
+import { GiFrenchFries,GiSlicedBread,GiStairsCake } from "react-icons/gi"
+import { RiDrinks2Fill } from "react-icons/ri"
 
 const Commands = () => {
 
@@ -53,6 +59,44 @@ const Commands = () => {
                     </label>
                 </div>
             {/* FIM ***********************************************/}
+
+                          {/* INÍCIO - ITENS DA COMANDA*/}
+                    <div className="general-group-itens-command">
+                        <div className="group-itens-command-selection"> {/* PRODUTOS PARA SELECIONAR */}
+                            <div className="area-icon-selection">
+                                <FaNotesMedical className="icon-selection"/>
+                                <span>ESTOQUE</span>
+                                
+                                    <Tooltip title="Clique aqui para exibir o estoque de LANCHES!">
+                                    <MdLunchDining className="icon-selection-lunch"/>
+                                    </Tooltip>
+                                    <Tooltip title="Clique aqui para exibir o estoque de PORÇÕES!">
+                                    <GiFrenchFries className="icon-selection-portion"/>
+                                    </Tooltip>
+                                    <Tooltip title="Clique aqui para exibir o estoque de PASTÉIS!">
+                                    <GiSlicedBread className="icon-selection-pastry"/>
+                                    </Tooltip>
+                                    <Tooltip title="Clique aqui para exibir o estoque de SOBREMESAS!">
+                                    <GiStairsCake className="icon-selection-dessert"/>
+                                    </Tooltip>
+                                    <Tooltip title="Clique aqui para exibir o estoque de INDUSTRIAIS!">
+                                    <FaCandyCane className="icon-selection-industrial"/>
+                                    </Tooltip>
+                                    <Tooltip title="Clique aqui para exibir o estoque de BEBIDAS!">
+                                    <RiDrinks2Fill className="icon-selection-drink"/>
+                                    </Tooltip>
+                                
+                            </div>
+                        </div>
+
+                        <div className="group-itens-command-cart"> {/* PRODUTOS SELECIONADOS */}
+                            <div className="area-icon-selection">
+                                <FaShoppingCart className="icon-selection"/>
+                                <span>CARRINHO</span>
+                            </div>
+                        </div>
+                    </div>
+                          {/* FIM - ITENS DA COMANDA*/}
 
          </form>
     </div>
